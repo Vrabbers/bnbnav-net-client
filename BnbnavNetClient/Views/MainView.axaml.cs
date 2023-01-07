a545f074eb54dd36c81ec27858628a110510d802
Vrabbers <25323861+Vrabbers@users.noreply.github.com>
2023-01-07 01:31:50 -0300

    Make night mode work in app controls

--- BnbnavNetClient/Views/MainView.axaml.cs
+++ BnbnavNetClient/Views/MainView.axaml.cs
@@ -1,13 +1,21 @@
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Interactivity;
+using Avalonia.Media;
+using Avalonia.Styling;
+using Avalonia.Themes.Fluent;
 using BnbnavNetClient.ViewModels;
 
 namespace BnbnavNetClient.Views;
 
 public partial class MainView : UserControl
 {
+    readonly Style _whiteTextStyle;
+
     public MainView()
     {
+        _whiteTextStyle = new Style(static x => x.OfType<TextBlock>());
+        _whiteTextStyle.Setters.Add(new Setter(TextBlock.ForegroundProperty, new SolidColorBrush(Colors.White)));
         InitializeComponent();
     }
 
@@ -18,4 +26,23 @@ public partial class MainView : UserControl
 
         await vm.InitMapService();
     }
+
+
+
+    public void ColorModeSwitch(object sender, RoutedEventArgs e)
+    {
+        var toggle = (ToggleButton)sender;
+        ((FluentTheme)App.Current!.Styles[0]).Mode = toggle.IsChecked ?? false ? FluentThemeMode.Dark : FluentThemeMode.Light;
+    
+        if (toggle.IsChecked ?? false)
+        {
+            //WASM seems to need a little help setting the textblock styles. hopefully they fix this sometime!
+            App.Current!.Styles.Add(_whiteTextStyle);
+        }
+        else
+        {
+            App.Current!.Styles.Remove(_whiteTextStyle);
+        }
+    }
+
 }
