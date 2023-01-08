683f7ee75bbe8dd44721c56e908eaadd18e76dd2
Victor Tran <vicr12345@gmail.com>
2023-01-08 21:07:24 +1100

    Change macOS CI

--- dist/build-macos.sh
+++ dist/build-macos.sh
@@ -9,12 +9,12 @@ RESOURCES_BUNDLE=$APP_BUNDLE/Contents/Resources
 
 rm -rf $APP_BUNDLE || true
 
-pushd BnbnavClient
-dotnet publish -r osx-$BUILD_ARCH --self-contained
+pushd BnbnavNetClient.Desktop
+dotnet publish -r osx-$BUILD_ARCH --self-contained -c release
 popd
 
 mkdir -p $APP_BUNDLE/Contents/MacOS
-cp -r "BnbnavClient/bin/Debug/net"*"/osx-$BUILD_ARCH/"* $APP_BUNDLE/Contents/MacOS
+cp -r "BnbnavNetClient.Desktop/bin/Release/net"*"/osx-$BUILD_ARCH/"* $APP_BUNDLE/Contents/MacOS
 mkdir -p $RESOURCES_BUNDLE
 #cp Distribution/icon.icns $RESOURCES_BUNDLE
 cp dist/Info.plist $APP_BUNDLE/Contents/
