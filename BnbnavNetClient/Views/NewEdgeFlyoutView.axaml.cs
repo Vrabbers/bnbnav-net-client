78e25c431012831e0b6ab24a05c080bc2a89f60e
Vrabbers <25323861+Vrabbers@users.noreply.github.com>
2023-01-15 21:24:26 -0300

    new edge flyout mockup

---
+++ BnbnavNetClient/Views/NewEdgeFlyoutView.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia.Controls;
+
+namespace BnbnavNetClient.Views;
+public partial class NewEdgeFlyoutView : UserControl
+{
+    public NewEdgeFlyoutView()
+    {
+        InitializeComponent();
+    }
+}
