85d0f55e9dd1c6d10ab585276bc17873c53f751a
Victor Tran <vicr12345@gmail.com>
2023-01-17 18:46:07 +1100

    Fix pluralisation

--- BnbnavNetClient.I18Next/CldrPluralResolver.cs
+++ BnbnavNetClient.I18Next/CldrPluralResolver.cs
@@ -20,15 +20,12 @@ public sealed class CldrPluralResolver : IPluralResolver
 
     public string GetPluralSuffix(string language, int count)
     {
-        //HACK: For some reason German doesn't seem to be working correctly so hardcode the English rules
-        var locale = Locale.Create(language.Split("-")[0]);
-
-        var pluralsFile = Assembly.GetAssembly(GetType())!.GetManifestResourceStream("BnbnavNetClient.I18n.Resources.plurals.xml")!;
+        var pluralsFile = Assembly.GetAssembly(GetType())!.GetManifestResourceStream("BnbnavNetClient.I18Next.Resources.plurals.xml")!;
         var xp = new XPathDocument(pluralsFile);
         
         var rules = new List<XPathDocument> { xp }
             .Elements("supplementalData/plurals[@type='cardinal']/pluralRules[contains(@locales, '" +
-                      locale.Id.Language.ToLowerInvariant() + "')]/pluralRule").Select<XPathNavigator, Rule>(Rule.Parse)
+                      language.Split("-")[0].ToLowerInvariant() + "')]/pluralRule").Select<XPathNavigator, Rule>(Rule.Parse)
             .OrderBy(x => Array.IndexOf(SuffixOrder, x.Category)).ToList();
 
         return $"_{rules.FirstOrDefault(rule => rule.Matches(RuleContext.Create(count)))?.Category ?? "other"}";
