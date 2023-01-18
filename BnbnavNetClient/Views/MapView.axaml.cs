b962e19324f504cdb9a7317e6a914145aa5bec80
Victor Tran <vicr12345@gmail.com>
2023-01-18 11:03:02 +1100

    Invalidate map state when WebSockets event occurs

--- BnbnavNetClient/Views/MapView.axaml.cs
+++ BnbnavNetClient/Views/MapView.axaml.cs
@@ -8,6 +8,7 @@ using ReactiveUI;
 using System;
 using System.IO;
 using System.Reactive;
+using BnbnavNetClient.Services;
 using Avalonia.Platform;
 using Avalonia.Svg.Skia;
 using Svg.Skia;
@@ -97,6 +98,9 @@ public partial class MapView : UserControl
         MapViewModel
             .WhenAnyPropertyChanged()
             .Subscribe(Observer.Create<MapViewModel?>(_ => { InvalidateVisual(); }));
+        MapViewModel.MapService
+            .WhenAnyPropertyChanged()
+            .Subscribe(Observer.Create<MapService?>(_ => InvalidateVisual()));
     }
 
     static readonly double LandmarkSize = 10;
