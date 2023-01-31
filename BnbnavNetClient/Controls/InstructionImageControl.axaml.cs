e414738c91e9817c6d907b019c1b7db2f4d62ee4
Victor Tran <vicr12345@gmail.com>
2023-01-31 23:28:57 +1100

    Implement correct roundabout drawing

--- BnbnavNetClient/Controls/InstructionImageControl.axaml.cs
+++ BnbnavNetClient/Controls/InstructionImageControl.axaml.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Numerics;
 using System.Reactive;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Shapes;
 using Avalonia.Media;
 using BnbnavNetClient.Helpers;
 using BnbnavNetClient.Models;
@@ -34,27 +36,89 @@ public class InstructionImageControl : TemplatedControl
         if (_Instruction is not null)
         {
             var bounds = Bounds;
-            var instructionFile = _Instruction.instructionType switch
+
+            if (_Instruction.roundaboutExit is not null && _Instruction.from is not null && _Instruction.to is not null)
             {
-                CalculatedRoute.Instruction.InstructionType.Departure => "depart",
-                CalculatedRoute.Instruction.InstructionType.Arrival => "arrive",
-                CalculatedRoute.Instruction.InstructionType.ContinueStraight => "continue-straight",
-                CalculatedRoute.Instruction.InstructionType.BearLeft => "bear-left",
-                CalculatedRoute.Instruction.InstructionType.TurnLeft => "turn-left",
-                CalculatedRoute.Instruction.InstructionType.SharpLeft => "sharp-left",
-                CalculatedRoute.Instruction.InstructionType.TurnAround => "turn-around",
-                CalculatedRoute.Instruction.InstructionType.SharpRight => "sharp-right",
-                CalculatedRoute.Instruction.InstructionType.TurnRight => "turn-right",
-                CalculatedRoute.Instruction.InstructionType.BearRight => "bear-right",
-                CalculatedRoute.Instruction.InstructionType.ExitLeft => "exit-left",
-                CalculatedRoute.Instruction.InstructionType.ExitRight => "exit-right",
-                CalculatedRoute.Instruction.InstructionType.Merge => "merge",
-                CalculatedRoute.Instruction.InstructionType.EnterRoundabout => "enter-roundabout",
-                CalculatedRoute.Instruction.InstructionType.LeaveRoundabout => "leave-roundabout",
-                _ => throw new ArgumentOutOfRangeException()
-            };
+                
+                var innerCircleBounds = bounds.Deflate(15);
+                var angle = _Instruction.from.Line.FlipDirection().AngleTo(_Instruction.roundaboutExit.Line) - 90;
+                var complex = Complex.FromPolarCoordinates(innerCircleBounds.Width / 2, MathHelper.ToRad(angle + 180));
+                var arcEnd = new Point(-complex.Real, complex.Imaginary) + innerCircleBounds.Center;
+                var arrowEnd = new ExtendedLine(arcEnd, new Point(arcEnd.X + 10, arcEnd.Y)).SetAngle(angle).Point2;
+                var sweepDirection = _Instruction.from.Line.FlipDirection().AngleTo(_Instruction.to.Line) < 0
+                    ? SweepDirection.Clockwise
+                    : SweepDirection.CounterClockwise;
+                
+                context.DrawEllipse(null, new Pen(new SolidColorBrush(new Color(100, 255, 255, 255)), 5), innerCircleBounds.Center, innerCircleBounds.Width / 2, innerCircleBounds.Height / 2);
+
+                var path = new PathGeometry
+                {
+                    Figures = new PathFigures
+                    {
+                        new()
+                        {
+                            StartPoint = new Point(bounds.Center.X, bounds.Bottom),
+                            Segments = new PathSegments
+                            {
+                                new LineSegment
+                                {
+                                    Point = new Point(innerCircleBounds.Center.X, innerCircleBounds.Bottom)
+                                },
+                                new ArcSegment
+                                {
+                                    IsLargeArc = sweepDirection == SweepDirection.Clockwise ? double.Abs(angle) <= 90 : double.Abs(angle) >= 90,
+                                    Point = arcEnd,
+                                    Size = innerCircleBounds.Size / 2,
+                                    SweepDirection = sweepDirection
+                                },
+                                new LineSegment
+                                {
+                                    Point = arrowEnd
+                                },
+                                new LineSegment
+                                {
+                                    Point = new ExtendedLine(arrowEnd, new Point(arrowEnd.X + 5, arrowEnd.Y)).SetAngle(angle - 135).Point2
+                                },
+                                new LineSegment
+                                {
+                                    Point = arrowEnd
+                                },
+                                new LineSegment
+                                {
+                                    Point = new ExtendedLine(arrowEnd, new Point(arrowEnd.X + 5, arrowEnd.Y)).SetAngle(angle + 135).Point2
+                                }
+                            },
+                            IsClosed = false
+                        }
+                    }
+                };
+
+                context.DrawGeometry(null, new Pen(new SolidColorBrush(new Color(255, 255, 255, 255)), 5, lineCap: PenLineCap.Round, lineJoin: PenLineJoin.Round), path);
+            }
+            else
+            {
+                var instructionFile = _Instruction.instructionType switch
+                {
+                    CalculatedRoute.Instruction.InstructionType.Departure => "depart",
+                    CalculatedRoute.Instruction.InstructionType.Arrival => "arrive",
+                    CalculatedRoute.Instruction.InstructionType.ContinueStraight => "continue-straight",
+                    CalculatedRoute.Instruction.InstructionType.BearLeft => "bear-left",
+                    CalculatedRoute.Instruction.InstructionType.TurnLeft => "turn-left",
+                    CalculatedRoute.Instruction.InstructionType.SharpLeft => "sharp-left",
+                    CalculatedRoute.Instruction.InstructionType.TurnAround => "turn-around",
+                    CalculatedRoute.Instruction.InstructionType.SharpRight => "sharp-right",
+                    CalculatedRoute.Instruction.InstructionType.TurnRight => "turn-right",
+                    CalculatedRoute.Instruction.InstructionType.BearRight => "bear-right",
+                    CalculatedRoute.Instruction.InstructionType.ExitLeft => "exit-left",
+                    CalculatedRoute.Instruction.InstructionType.ExitRight => "exit-right",
+                    CalculatedRoute.Instruction.InstructionType.Merge => "merge",
+                    CalculatedRoute.Instruction.InstructionType.EnterRoundabout => "enter-roundabout",
+                    CalculatedRoute.Instruction.InstructionType.LeaveRoundabout => "leave-roundabout",
+                    _ => throw new ArgumentOutOfRangeException()
+                };
             
-            context.DrawSvgUrl($"avares://BnbnavNetClient/Assets/Instructions/{instructionFile}.svg", bounds);
+                context.DrawSvgUrl($"avares://BnbnavNetClient/Assets/Instructions/{instructionFile}.svg", bounds);
+            }
         }
     }
 }
